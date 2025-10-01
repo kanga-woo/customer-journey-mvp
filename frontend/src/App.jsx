d7d08b15d5dcc9959fe3e4908d096b4c97a8c9e4
kanga-woo <peterwoo903@gmail.com>
2025-09-30 19:56:17 -0500

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CustomerSearch from './components/CustomerSearch'
+
+
+export default function App(){
+return (
+<div style={{padding:20}}>
+<h1>Customer Journey MVP</h1>
+<CustomerSearch />
+</div>
+)
+}
