0de9d3bf9e803dff71521528d3198ccdfcda2fe3
kanga-woo <peterwoo903@gmail.com>
2025-09-30 19:40:24 -0500

    Create webhooks.py

---
+++ backend/app/webhooks.py
@@ -0,0 +1,11 @@
+def normalize_mailchimp(payload):
+# return a list of EventPayload-like dicts
+events = []
+# Add examples depending on payload structure
+return events
+
+
+def normalize_zendesk_ticket(payload):
+events = []
+# extract ticket text, customer id, severity
+return events
